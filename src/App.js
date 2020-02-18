b7064858b042bcb932fdc2fb09173426972d5a50
Adrijana Posedel <adry1006@gmail.com>
2020-02-18 16:51:14 +0000

    set up layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { questionList } from './Questions';
 
 function App() {
+  console.log('questions', questionList[0]);
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src='https://image.flaticon.com/icons/png/512/417/417174.png' className="App-logo seagull" alt="seagull" />
+        <img src='https://image.flaticon.com/icons/png/512/417/417166.png' className='App-logo lighthouse' alt='lighthouse' />
+        <div className="quiz-section">
+        <ul class="flex-container row">
+          <li class="quiz-item">
+            <img src="https://image.flaticon.com/icons/svg/995/995179.svg" className="App-icon" alt="pineapple" />
+            <p>pineapple</p>
+          </li>
+          <li class="quiz-item">
+            <img src="https://image.flaticon.com/icons/svg/995/995179.svg" className="App-icon" alt="pineapple" />
+            <p>pineapple</p>
+          </li>
+          <li class="quiz-item">
+            <img src="https://image.flaticon.com/icons/svg/995/995179.svg" className="App-icon" alt="pineapple" />
+            <p>pineapple</p>
+          </li>
+        </ul>
+          {/* <p>
+            The impossible Croatian quiz!
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn Croatian!
+          </a> */}
+        </div>
       </header>
     </div>
   );
