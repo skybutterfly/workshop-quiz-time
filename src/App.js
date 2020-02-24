b224efecd0d950f6367be22e94e132e3a751266a
Adrijana Posedel <adry1006@gmail.com>
2020-02-24 19:08:39 +0000

    populating questions and options

--- src/App.js
+++ src/App.js
@@ -1,45 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { questionList } from './Questions';
 
-function App() {
-  console.log('questions', questionList[0]);
-  
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src='https://image.flaticon.com/icons/png/512/417/417174.png' className="App-logo seagull" alt="seagull" />
-        <img src='https://image.flaticon.com/icons/png/512/417/417166.png' className='App-logo lighthouse' alt='lighthouse' />
-        <div className="quiz-section">
-        <ul class="flex-container row">
-          <li class="quiz-item">
-            <img src="https://image.flaticon.com/icons/svg/995/995179.svg" className="App-icon" alt="pineapple" />
-            <p>pineapple</p>
-          </li>
-          <li class="quiz-item">
-            <img src="https://image.flaticon.com/icons/svg/995/995179.svg" className="App-icon" alt="pineapple" />
-            <p>pineapple</p>
-          </li>
-          <li class="quiz-item">
-            <img src="https://image.flaticon.com/icons/svg/995/995179.svg" className="App-icon" alt="pineapple" />
-            <p>pineapple</p>
-          </li>
-        </ul>
-          {/* <p>
-            The impossible Croatian quiz!
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Croatian!
-          </a> */}
+class App extends Component {
+  state = {
+    step: 0,
+    nextQuestion: false,
+    selectedAnswers: [],
+    questionTotal: 0,
+    optionSelected: false,
+  };
+
+  nextQuestion = () => {
+    this.setState({ step: this.state.step + 1 })
+    console.log('Next!', this.state.step);
+  }
+
+  render() {
+    const { step } = this.state;
+    const questionLen = questionList.length;
+    const lastQuestion = questionLen !== (step + 1);
+    console.log('questionNo', questionLen, (step + 1), lastQuestion)
+    console.log('q', questionList[step]);
+    
+    return (
+      <div className="App">
+        <div className="App-section">
+          <img src='https://image.flaticon.com/icons/png/512/417/417174.png' className="App-logo seagull" alt="seagull" />
+          <img src='https://image.flaticon.com/icons/png/512/417/417166.png' className='App-logo lighthouse' alt='lighthouse' />
+          <div className="quiz-section">
+            <div className="row">
+              <div className="question-area">
+              <h1>The (im)possible Croatian quiz!</h1>
+              <h3>Find the</h3>
+              <p>{ questionList[step].question }</p>
+              </div>
+            </div>
+          </div>
         </div>
-      </header>
-    </div>
-  );
+        <div className="App-section">
+          <ul className="flex-container row">
+            { questionList[step].options.map((item, i) => 
+              <li key={i} className="quiz-item">
+                <img src="https://image.flaticon.com/icons/svg/995/995179.svg" className="App-icon" alt="pineapple" />
+                <p>{item.option}</p>
+              </li>
+              ) 
+            }
+            </ul>
+            { lastQuestion && <button onClick={() => this.nextQuestion()}>Next</button> }
+        </div>
+      </div>
+    );
+  }
+
 }
 
 export default App;
